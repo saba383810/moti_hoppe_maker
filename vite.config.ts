6167ee35aa43fcbdbd3fc9346b7921045c951794
saba383810 <saba383810@gmail.com>
2026-07-13 23:18:54 +0900

    プロジェクト足場: Vite + TypeScript + GitHub Pages デプロイworkflow

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+
+// GitHub Pages: https://saba383810.github.io/moti_hoppe_maker/
+export default defineConfig({
+  base: '/moti_hoppe_maker/',
+  build: {
+    target: 'es2022',
+  },
+});
